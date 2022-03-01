25aaa540f4c3e82fccca1c40cdd0a16c24cfa0f3
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2022-03-01 15:49:32 +0530

    ecommerce

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -190,8 +190,28 @@ exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
 exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
   const newUserData = {
     name: req.body.name,
+    email: req.body.email,
 
   };
+  if (req.body.avatar !== "") {
+    const user = await User.findById(req.user.id);
+
+    const imageId = user.avatar.public_id;
+
+    await cloudinary.v2.uploader.destroy(imageId);
+    //destroy is used to delete
+
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+      folder: "avatars",
+      width: 150,
+      crop: "scale",
+    });
+
+    newUserData.avatar = {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    };
+  }
 
   
   const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
