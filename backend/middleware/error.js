363e7fdf2355b42529f52e19045f66099be040f7
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2022-04-04 23:19:15 +0530

    worked on forgot password

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,31 +1,28 @@
 const ErrorHandler = require("../utils/errorhander");
 
-module.exports=(err,req,res,next)=>{
-    err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal Server Error";
-    
-    
-    //Wrong mongodb ID errors
-    if(err.name==="CastError"){
-        const message=`Resource not found. Invalid: ${err.path}`;
-        err=new ErrorHandler(message,400);
-    }
-    
-    
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
 
- //Mongoose duplicate key error
- if (err.code === 11000) {
+  // Wrong Mongodb Id error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Mongoose duplicate key error
+  if (err.code === 11000) {
     const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
     err = new ErrorHandler(message, 400);
   }
 
-//   Wrong JWT error
+  // Wrong JWT error
   if (err.name === "JsonWebTokenError") {
     const message = `Json Web Token is invalid, Try again `;
     err = new ErrorHandler(message, 400);
   }
 
-//   JWT EXPIRE error
+  // JWT EXPIRE error
   if (err.name === "TokenExpiredError") {
     const message = `Json Web Token is Expired, Try again `;
     err = new ErrorHandler(message, 400);
