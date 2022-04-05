7b4532d9304dc9b2010d074e2a7634fa8dc66594
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2022-04-05 11:45:20 +0530

    worked on process/payment route

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,15 @@ const app = express();
 const cookieParser=require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileupload = require("express-fileupload");
+const path = require("path");
+const errorMiddleware = require("./middleware/error");
 
-const errorMiddleware=require("./middleware/error");
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({ path: "backend/config/config.env" });
+}  
+
+// const errorMiddleware=require("./middleware/error");
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}))
@@ -22,5 +29,55 @@ app.use("/api/v1",product);
 app.use("/api/v1",user);
 app.use("/api/v1",order);
 app.use("/api/v1",payment);
+// app.use(errorMiddleware);
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
+// Middleware for Errors
 app.use(errorMiddleware);
 module.exports=app;
+
+
+// const express = require("express");
+// const app = express();
+// const cookieParser = require("cookie-parser");
+// const bodyParser = require("body-parser");
+// const fileUpload = require("express-fileupload");
+// const path = require("path");
+
+// const errorMiddleware = require("./middleware/error");
+
+// Config
+// if (process.env.NODE_ENV !== "PRODUCTION") {
+//   require("dotenv").config({ path: "backend/config/config.env" });
+// }
+
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(fileUpload());
+
+// Route Imports
+// const product = require("./routes/productRoute");
+// const user = require("./routes/userRoute");
+// const order = require("./routes/orderRoute");
+// const payment = require("./routes/paymentRoute");
+
+// app.use("/api/v1", product);
+// app.use("/api/v1", user);
+// app.use("/api/v1", order);
+// app.use("/api/v1", payment);
+
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
+
+// Middleware for Errors
+// app.use(errorMiddleware);
+
+// module.exports = app;
