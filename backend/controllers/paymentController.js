b2f6b256bd0f6519b422b045f3b84b25462bdc09
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2022-04-05 12:00:58 +0530

    worked on process/payment route

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -3,13 +3,13 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
-  // const myPayment = await stripe.paymentIntents.create({
-  //   amount: req.body.amount,
-  //   currency: "inr",
-  //   metadata: {
-  //     company: "BigMart",
-  //   },
-  // });
+  const myPayment = await stripe.paymentIntents.create({
+    amount: req.body.amount,
+    currency: "inr",
+    metadata: {
+      company: "BigMart",
+    },
+  });
   console.log("hellooooo"+req.body);
 
   res
